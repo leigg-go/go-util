96fc044b9d85510696932982ecd487dd00501180
lilei <random2035@qq.com>
2020-09-27 17:20:10 +0800

    optimize

---
+++ _lock/in_redis.go
@@ -0,0 +1,87 @@
+package _lock
+
+import (
+	"errors"
+	"github.com/go-redis/redis"
+	"sync"
+	"time"
+)
+
+// Distributed Lock in redis.
+
+type DistributedLock interface {
+	Lock(timeout time.Duration) error
+	UnLock() error
+}
+
+type DistributedLockInRedis struct {
+	redisC *redis.Client
+	key    string
+	value  []byte
+	expire time.Duration
+	// mutex allow to share a Lock in multi goroutines
+	mu sync.RWMutex
+	// `isLocked` maintains lock state in currency goroutine, it avoids unlocking lock
+	// that do not belong to currency goroutine.
+	isLocked bool
+}
+
+func NewDistributedLockByRedis(c *redis.Client, k string, v []byte, expire time.Duration) DistributedLock {
+	return &DistributedLockInRedis{redisC: c, key: k, value: v, expire: expire}
+}
+
+var (
+	TimeoutErr         = errors.New("DistributedLockInRedis: acquire lock timeout")
+	AlreadyAcquiredErr = errors.New("DistributedLockInRedis: already acquired lock")
+	NotLockedErr       = errors.New("DistributedLockInRedis: acquire lock failed")
+)
+
+// Lock try to acquire `lock`. If the lock has been acquired, it will return AlreadyAcquiredErr
+func (l *DistributedLockInRedis) Lock(timeout time.Duration) error {
+	if l.Locked() {
+		return AlreadyAcquiredErr
+	}
+	pollIntvl := time.NewTicker(time.Millisecond * 10)
+	defer pollIntvl.Stop()
+	deadline := time.Now().Add(timeout)
+
+	for {
+		if time.Now().Sub(deadline) >= 0 {
+			return TimeoutErr
+		}
+
+		r := l.redisC.SetNX(l.key, l.value, l.expire)
+		if r.Err() != nil {
+			return r.Err()
+		}
+		if r.Val() {
+			l.setLockState(true)
+			return nil
+		}
+		<-pollIntvl.C
+	}
+}
+
+// Lock try to release `lock`. If the lock has not been acquired, it will return NotLockedErr
+func (l *DistributedLockInRedis) UnLock() error {
+	if l.Locked() {
+		if err := l.redisC.Del(l.key).Err(); err != nil {
+			return err
+		}
+		l.setLockState(false)
+		return nil
+	}
+	return NotLockedErr
+}
+
+func (l *DistributedLockInRedis) Locked() bool {
+	l.mu.RLock()
+	defer l.mu.RUnlock()
+	return l.isLocked
+}
+
+func (l *DistributedLockInRedis) setLockState(state bool) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	l.isLocked = state
+}
--- test/lock/redis_lock_test.go
+++ test/lock/redis_lock_test.go
@@ -14,7 +14,7 @@ import (
 
 var opts = &redis.Options{
 	Addr:         "127.0.0.1:6379",
-	Password:     "",
+	Password:     "123",
 	DB:           0,
 	DialTimeout:  2 * time.Second,
 	ReadTimeout:  3 * time.Second,
@@ -40,6 +40,7 @@ func NoCurrencyTask(t *testing.T, i int, wg *sync.WaitGroup) {
 			wg.Done()
 		}
 	}()
+	// 每个goroutine应该独享一个lock对象，但是lock key是相同的
 	lock := _lock.NewDistributedLockByRedis(_redis.DefClient, DistributedLockInRedisKey, nil, DistributedLockInRedisExpire)
 	// 获取锁操作需设置超时，因为是通过网络, 注意这个超时不要设置太小(需大于正常操作耗时)
 	err := lock.Lock(time.Millisecond * 50)
@@ -70,7 +71,6 @@ func TestNewDistributedLockInRedis(t *testing.T) {
 	assert.Equal(t, globalCounter, 1)
 
 	// 上面的所有goroutine应该释放锁，下面再执行一次，应该可以获得锁
-	wg.Add(1)
 	NoCurrencyTask(t, 1000, nil)
 	assert.Equal(t, globalCounter, 2)
 }
