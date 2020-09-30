474999e5b12befc2e007da5e810199b4baffa147
lilei <random2035@qq.com>
2020-09-30 20:31:51 +0800

    better

--- test/lock/redis_lock_test.go
+++ test/lock/redis_lock_test.go
@@ -26,7 +26,7 @@ var opts = &redis.Options{
 var globalCounter int
 
 // N个goroutine并发，但最终只有一个得到锁，并执行 globalCounter++
-var goroutineCount = 500
+var goroutineCount = 100
 
 // Lock共享redis-key
 const DistributedLockInRedisKey = "DistributedLockInRedis"
@@ -34,14 +34,13 @@ const DistributedLockInRedisKey = "DistributedLockInRedis"
 // expire值应该略大于task执行耗时
 const DistributedLockInRedisExpire = time.Second * 5
 
-func NoCurrencyTask(t *testing.T, i int, wg *sync.WaitGroup) {
+func noCurrencyTask(t *testing.T, i int, wg *sync.WaitGroup, lock _lock.DistributedLock) {
 	defer func() {
 		if wg != nil {
 			wg.Done()
 		}
 	}()
 	// 每个goroutine应该独享一个lock对象，但是lock key是相同的
-	lock := _lock.NewDistributedLockByRedis(_redis.DefClient, DistributedLockInRedisKey, nil, DistributedLockInRedisExpire)
 	// 获取锁操作需设置超时，因为是通过网络, 注意这个超时不要设置太小(需大于正常操作耗时)
 	err := lock.Lock(time.Millisecond * 50)
 	if err != nil {
@@ -49,7 +48,7 @@ func NoCurrencyTask(t *testing.T, i int, wg *sync.WaitGroup) {
 		log.Printf("goroutine %d lock timeout!", i)
 		return
 	}
-	log.Printf("goroutine %d got the rights!", i)
+	log.Printf("goroutine %d got the lock!", i)
 	// 模拟耗时，届时只有一个协程会获得锁，其他均超时
 	time.Sleep(time.Second * 1)
 
@@ -62,15 +61,40 @@ func TestNewDistributedLockInRedis(t *testing.T) {
 	_redis.MustInitDefClient(opts)
 	defer _redis.Close()
 
+	lock := _lock.NewDistributedLockByRedis(_redis.DefClient, DistributedLockInRedisKey, nil, DistributedLockInRedisExpire)
+
 	var wg sync.WaitGroup
 	for i := 0; i < goroutineCount; i++ {
 		wg.Add(1)
-		go NoCurrencyTask(t, i, &wg)
+		// 不同的协程应该使用一个同key不同对象的lock，一个协程只能释放自己加的锁
+		go noCurrencyTask(t, i, &wg, lock.Copy())
 	}
 	wg.Wait()
 	assert.Equal(t, globalCounter, 1)
 
 	// 上面的所有goroutine应该释放锁，下面再执行一次，应该可以获得锁
-	NoCurrencyTask(t, 1000, nil)
+	noCurrencyTask(t, goroutineCount+1, nil, lock.Copy())
 	assert.Equal(t, globalCounter, 2)
+}
+
+func TestLockErr(t *testing.T) {
+	_redis.MustInitDefClient(opts)
+	defer _redis.Close()
+	// 每个goroutine应该独享一个lock对象，但是lock key是相同的
+	lock := _lock.NewDistributedLockByRedis(_redis.DefClient, DistributedLockInRedisKey, nil, DistributedLockInRedisExpire)
+	// 获取锁操作需设置超时，因为是通过网络, 注意这个超时不要设置太小(需大于正常操作耗时)
+	err := lock.Lock(time.Millisecond * 50)
+	if err != nil {
+		t.Fatalf("Should be nil, but got err:%v", err)
+	}
+	// 上面还没释放锁，同一个内存对象再去获取锁，就会panic
+	err = lock.Lock(time.Millisecond * 50)
+	assert.Equal(t, _lock.AlreadyAcquiredErr, err)
+
+	err = lock.UnLock()
+	if err != nil {
+		t.Fatalf("Should be nil, but got err:%v", err)
+	}
+	err = lock.UnLock()
+	assert.Equal(t, _lock.NotLockedErr, err)
 }
